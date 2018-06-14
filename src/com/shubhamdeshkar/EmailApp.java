1e00b42469c515a5c63ca327bca8f144e5e4a4f9
Shubham Deshkar <shubham_deshkar@yahoo.com>
2018-06-14 11:07:22 -0400

    Initialize EmailApp.java

---
+++ src/com/shubhamdeshkar/EmailApp.java
@@ -0,0 +1,23 @@
+package com.shubhamdeshkar;
+
+public class EmailApp {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int storageCapacity;
+    private String department;
+    private String altenateEmail;
+
+    // Constructor
+
+    // Ask for department
+
+    // Generate a random Password
+
+    // Set the mailbox storageCapacity
+
+    // Set alternateEmail
+
+    // Change password
+}
