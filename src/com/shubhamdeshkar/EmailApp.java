bc29dc3e8ca0d9a031665d0894d138de8773afb5
Shubham Deshkar <shubham_deshkar@yahoo.com>
2018-06-14 11:09:38 -0400

    Add a constructor for EmailApp

--- src/com/shubhamdeshkar/EmailApp.java
+++ src/com/shubhamdeshkar/EmailApp.java
@@ -10,6 +10,11 @@ public class EmailApp {
     private String altenateEmail;
 
     // Constructor
+    public EmailApp(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL INITIALIZED FOR: " + this.firstName + " " + this.lastName);
+    }
 
     // Ask for department
 
